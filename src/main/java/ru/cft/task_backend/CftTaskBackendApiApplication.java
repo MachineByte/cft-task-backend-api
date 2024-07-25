87ce514849c820958fd21d2ff9c3a073523e0f55
Михаил Белянов <mihail.belyanov@gmail.com>
2024-07-25 13:25:43 +0700

    [NEW] Initial commit. Created package structure

---
+++ src/main/java/ru/cft/task_backend/CftTaskBackendApiApplication.java
@@ -0,0 +1,13 @@
+package ru.cft.task_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CftTaskBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CftTaskBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/cft/task_backend/CftTaskBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cft.task_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CftTaskBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
